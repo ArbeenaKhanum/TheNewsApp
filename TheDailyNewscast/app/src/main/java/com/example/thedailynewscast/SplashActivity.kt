fb31414de29920693c825fe26c90a514c64a203b
ArbeenaKhanum <arbeenaak@gmail.com>
2021-01-04 20:23:15 +0530

    splash activity added

---
+++ TheDailyNewscast/app/src/main/java/com/example/thedailynewscast/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.example.thedailynewscast
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val runnable = Runnable {
+            val splashIntent = Intent(this, MainActivity::class.java)
+            startActivity(splashIntent)
+            finish()
+        }
+        val handler = Handler()
+        handler.postDelayed(runnable, 3000)
+
+        animationOnSplash()
+    }
+
+    private fun animationOnSplash() {
+        val splashAnimation = AnimationUtils.loadAnimation(this, R.anim.transitions_splash)
+        val lTRAnimation = AnimationUtils.loadAnimation(this, R.anim.left_to_right)
+        val rTLAnimation = AnimationUtils.loadAnimation(this, R.anim.right_to_left)
+        tvTheDaily.animation = lTRAnimation
+        ivNewsLogo.animation = splashAnimation
+        tvNewscast.animation = rTLAnimation
+    }
+}
